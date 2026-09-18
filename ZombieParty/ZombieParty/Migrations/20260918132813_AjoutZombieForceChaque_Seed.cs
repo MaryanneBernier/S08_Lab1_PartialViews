d0f81332d23129dfa9744c0aab00ea4ccdf7c773
Maryanne Bernier <maryannebernier@hotmail.com>
2026-09-18 09:30:44 -0400

    FCT Ajout Zombie Force et seed
    Ajout de la propriété Force dans Zombie, modification du Seed et des vues Create, Update et Details pour ajouter le champ Force

---
+++ ZombieParty/ZombieParty/Migrations/20260918132813_AjoutZombieForceChaque_Seed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZombieParty.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjoutZombieForceChaque_Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
